2d4f07cca192ee8067b5318f49cf2f4ccbef6166
Bhongo <sinxo.bhongolethu@gmail.com>
2022-06-05 13:55:06 +0200

    1 file modified

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -1,35 +1,45 @@
 const mathOperations = require('../src/calculator');
-
+ 
 describe("Calculator Tests", () => {
-  test("Addition of 2 numbers", () => {
-    // arrange and act
-    var result = mathOperations.sum(1,2)
 
-    // assert
-    expect(result).toBe(3);
-  });
+ test("Addition of 2 numbers", () => {
 
-  test("Subtraction of 2 numbers", () => {
-  // arrange and act
-  var result = mathOperations.diff(10,2)
+   // arrange and act
+   var result = mathOperations.sum(1,2)
 
-  // assert
-  expect(result).toBe(8);
-  });
+   // assert
+   expect(result).toBe(3);
 
-  test("Multiplication of 2 numbers", () => {
-    // arrange and act
-    var result = mathOperations.product(2,8)
+ });
 
-    // assert
-    expect(result).toBe(16);
-  });
+ test("Subtraction of 2 numbers", () => {
 
-  test("Division of 2 numbers", () => {
-    // arrange and act
-    var result = mathOperations.divide(24,8)
+   // arrange and act
+   var result = mathOperations.diff(10,2);
 
-    // assert
-    expect(result).toBe(3);
-  });
+   // assert
+   expect(result).toBe(21); //updated to fail
+
+ });
+
+
+ test("Multiplication of 2 numbers", () => {
+
+   // arrange and act
+   var result = mathOperations.product(2,8)
+
+   // assert
+   expect(result).toBe(10); //updated to fail
+
+ });
+
+ test("Division of 2 numbers", () => {
+  // arrange and act
+  var result = mathOperations.divide(24,8);
+
+  // assert
+  expect(result).toBe(3);
+
+});
+
 });
